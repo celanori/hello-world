1e3d021e38d724acf57128387734e3d3164d61ba
celanori <celanorri@gmail.com>
2017-07-21 11:53:28 -0500

    New file, not branch :/
    This is where this is supposed o go

---
+++ Exercise2.py
@@ -0,0 +1,6 @@
+mysteryNumber = int(input("Please enter a whole number: "))
+remainderNumber = mysteryNumber % 2 
+if remainderNumber == 0:
+      print("The input number ", mysteryNumber, " even.")
+else:
+      print("The input number", mysteryNumber, "odd.") 
