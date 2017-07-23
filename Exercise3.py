cfe6f1edb2b6c387a635f708ce43257ec090ff2a
celanori <celanorri@gmail.com>
2017-07-23 13:48:41 -0500

    Exercise3.py
    Exercise 3 first solution.

---
+++ Exercise3.py
@@ -0,0 +1,9 @@
+i = 0
+mainList  = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89];
+while i < 11:
+  if mainList[i] < 5:
+      print("The value: ", mainList[i], "is lower then 5.")
+      i+=1
+  else:
+      print("The value: ", mainList[i], "is equal or more then 5.")
+      i+=1
