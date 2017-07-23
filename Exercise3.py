0ab449f21086c82dee952c881e466e05f9ab003c
celanori <celanorri@gmail.com>
2017-07-23 13:58:30 -0500

    Update Exercise3.py
    Added addition

--- Exercise3.py
+++ Exercise3.py
@@ -1,9 +1,13 @@
 i = 0
 mainList  = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89];
+newListUnderFive = [];
+newListOverFive = [];
 while i < 11:
   if mainList[i] < 5:
-      print("The value: ", mainList[i], "is lower then 5.")
+      newListUnderFive += [mainList[i]]
       i+=1
   else:
-      print("The value: ", mainList[i], "is equal or more then 5.")
+      newListOverFive += [mainList[i]]
       i+=1
+print(newListUnderFive)
+print(newListOverFive)
