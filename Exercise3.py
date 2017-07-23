d73e149d13290e02ceb980e6866759c99cb63eca
celanori <celanorri@gmail.com>
2017-07-23 14:03:58 -0500

    Update Exercise3.py
    Written all on one line and simplified, not the greatest work though.

--- Exercise3.py
+++ Exercise3.py
@@ -1,13 +1,1 @@
-i = 0
-mainList  = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89];
-newListUnderFive = [];
-newListOverFive = [];
-while i < 11:
-  if mainList[i] < 5:
-      newListUnderFive += [mainList[i]]
-      i+=1
-  else:
-      newListOverFive += [mainList[i]]
-      i+=1
-print(newListUnderFive)
-print(newListOverFive)
+mainList  = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]; newListUnderFive = []; newListOverFive = []; newListUnderFive += [mainList[0:4]]; newListOverFive += [mainList[4:11]]; print(newListUnderFive); print(newListOverFive)
