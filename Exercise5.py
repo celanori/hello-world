086c50979d61ece855406ac7406e681a972e6e61
celanori <celanorri@gmail.com>
2017-07-24 13:56:38 -0500

    Update Exercise5.py
    Added random lists.

--- Exercise5.py
+++ Exercise5.py
@@ -1,7 +1,9 @@
-firstList = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
-secondList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+import random
+firstList = random.sample(range(30),8)
+secondList = random.sample(range(30),10)
 indexList = range(0,1000)
+print(firstList)
+print(secondList)
 for i in indexList:
     if i in secondList and i in firstList:
         print(i, "is in both lists") 
-            
